b5302dcef8b296a1f3f34478b755beaa48bed4cb
Tam Tran <ttran244@uwo.ca>
2015-02-06 21:22:43 +0000

    prompt done

--- asn1/shell.c
+++ asn1/shell.c
@@ -10,12 +10,23 @@
  * and executes programs based on the input.
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 int main(int argc, char** argv)
 {
-
-
-
-
-
-
+    char input;
+    char name = NULL;
+    while (name == NULL) 
+    {
+      printf("Enter your name:\n");
+      scanf("%s", &name);
+    }
+    while (1)
+    {
+      printf("%s>", &name);
+      scanf("%s", &input);
+    }
+  return 0;
 }
